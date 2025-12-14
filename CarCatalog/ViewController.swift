2bddc298d359b3a54de4e95d810350b56890e821
LeonidPirogov <lv.pirogof@gmail.com>
2025-12-14 09:55:23 +0300

    Initial Commit

---
+++ CarCatalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarCatalog
+//
+//  Created by Leonid on 14.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
